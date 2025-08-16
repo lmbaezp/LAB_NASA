624c920383996bde2a6b86381b7f64b5db32141e
LMBP <juenpame@gmail.com>
2025-08-15 21:18:21 -0500

    final lab img NASA

--- main.js
+++ main.js
@@ -60,28 +60,67 @@ function mostrarFoto(resultadoSolicitud) {
     contenedor.innerHTML = '';
 
     const idBtn = 'favBtn_' + Date.now();
+    const idBtnModal = 'favBtnModal' + Date.now();
 
     contenedor.innerHTML = `
-        <div class="container text-center">
+        <div class="container text-center d-none d-lg-block">
             <div class="row justify-content-center">
                 <p class="fs-3">${resultadoSolicitud.title}</p>
                 <p>${resultadoSolicitud.date}</p>
             </div>
-            <div class="row align-items-center mt-3">
-                <div class="col-6">
-                    <img src="${resultadoSolicitud.url}" class="img-fluid text-center" alt="...">
+            <div class="row justify-content-center align-items-center mt-3">
+                <div id="foto-busq" class="col-6">
+                    <img src="${resultadoSolicitud.url}" class="img-fluid" alt="${resultadoSolicitud.title}">
                 </div>
                 <div class="col-6">
-                    <p class="card-text">${resultadoSolicitud.explanation}</p>
+                    <p class="card-text text-start">${resultadoSolicitud.explanation}</p>
                 </div>
             </div>
             <div class="row justify-content-center mt-2">
                 <button type="button" id="${idBtn}" class="btn btn-guardar mt-3 col-6">Guardar en mis favoritos</button>
             </div>
-        </div>                                
+        </div> 
+        
+        <div id="card-busq" class="card text-bg-dark rounded border border-0 p-0 d-inline-flex d-lg-none mx-auto">
+            <img id="card-img-busq" src="${resultadoSolicitud.url}" class="card-img" alt="${resultadoSolicitud.title}">
+            <div class="card-img-overlay">
+                <div class="card-title text-start">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-card p-1" data-bs-toggle="modal" data-bs-target="#busqBtn_${Date.now()}">
+                    Ver más
+                </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center mt-2 d-inline-flex d-lg-none mx-auto">
+            <button type="button" id="${idBtnModal}" class="btn btn-guardar mt-3 col-6">Guardar en mis favoritos</button>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="busqBtn_${Date.now()}">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header pb-0">
+                    <div>
+                        <p class="fw-bold mb-0" id="exampleModalLabel">${resultadoSolicitud.title}</p>
+                        <p class="mx-3">${resultadoSolicitud.date}</p>
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-start">${resultadoSolicitud.explanation}</p>
+                </div>
+            </div>
+        </div>
+        </div>
     `;
 
     document.getElementById(idBtn).addEventListener('click', () => {
+        guardarFavorito(resultadoSolicitud.title, resultadoSolicitud.explanation, resultadoSolicitud.url, resultadoSolicitud.date);
+    });
+
+    document.getElementById(idBtnModal).addEventListener('click', () => {
         guardarFavorito(resultadoSolicitud.title, resultadoSolicitud.explanation, resultadoSolicitud.url, resultadoSolicitud.date);
     });
 
@@ -102,9 +141,21 @@ function guardarFavorito(ftitle, fexplanation, furl, fdate) {
     if (!favoritos.find(f => f.fecha === fdate)) {
         favoritos.push({ titulo: ftitle, explicacion: fexplanation, url: furl, fecha: fdate });
         localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    } else {
+        Swal.fire({
+            text: "La imagen ya está en favoritos",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            returnFocus: false
+        });
     }
-
     cargarFavoritos();
+    document.getElementById("favoritos").scrollIntoView({
+        behavior: "smooth" 
+    });
 }
 
 
@@ -135,11 +186,11 @@ function cargarFavoritos() {
     if (favoritos.length > 0) {
         favoritos.forEach(f => {
             const divCard = document.createElement('div');
-            divCard.classList.add('col', 'm-0')
+            divCard.classList.add('col', 'mx-0', 'cards-favorites', 'rounded')
 
             divCard.innerHTML = `
         
-                <div class="card text-bg-dark h-100">
+                <div class="card text-bg-dark rounded h-100 card-favorite">
                     <img src="${f.url}" class="card-img h-100" alt="${f.title}">
                     <div class="card-img-overlay">
                         <div class="card-title text-start">
@@ -153,7 +204,7 @@ function cargarFavoritos() {
         
                 <!-- Modal -->
                 <div class="modal fade" id="favBtn_${Date.now()}">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header pb-0">
                             <div>
@@ -185,7 +236,7 @@ function cargarFavoritos() {
 *****************************************************/
 document.addEventListener('DOMContentLoaded', async function () {
     const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${fechaActual}`;
-    
+
     const resultadoSolicitud = await solicitudAPI(url);
     if (typeof resultadoSolicitud === "string") {
         console.error(resultadoSolicitud);
@@ -193,6 +244,6 @@ document.addEventListener('DOMContentLoaded', async function () {
     else if (typeof resultadoSolicitud === "object") {
         mostrarFoto(resultadoSolicitud);
     }
-    
+
     mostrarFoto(resultadoSolicitud);
 });
